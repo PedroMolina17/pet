e3a5796e3cf51e33f7de6e093a6792dc2d38a937
pedromolina17 <pedro.molinanoa@gmail.com>
2024-01-31 00:42:37 -0500

    pet li

--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -4,23 +4,23 @@ import { IoPawSharp } from "react-icons/io5";
 
 const Pet = () => {
   return (
-    <div className="flex col-2 w-full h-screen">
-      <div className="flex flex-col w-1/4 bg-[#0e1013] col-span-1  ">
-        <div className="flex w-full justify-between items-center  py-7 px-4 bg-gradient-to-tr from-[#fd2878] to-[#ff5c3b]">
-          <div> Usuario </div>
+    <div className="flex col-5 w-full h-screen">
+      <div className="flex flex-col w-1/4 min-w-64 bg-[#0e1013] col-span-1  max-md:hidden border-r border-[#a0a8b1] ">
+        <div className="flex w-full justify-between items-center  py-6 px-4 bg-gradient-to-tr from-[#fd2878] to-[#ff5c3b]">
+          <div className="font-bold"> Usuario </div>
           <div className="rounded-full flex gap-y-1 gap-x-4 ">
             <div className="rounded-full bg-[#3e1c1f] p-2 flex">
-              <IoPawSharp className="text-2xl text-white hover:text-[#2a9eff] hover:transition-opacity" />
+              <IoPawSharp className="text-2xl text-white transition-colors duration-300 hover:text-red-500 " />
             </div>
             <div className="rounded-full bg-[#3e1c1f] p-2 flex">
-              <FaSuitcase className="text-2xl hover:text-[#2a9eff] hover:transition-opacity" />
+              <FaSuitcase className="text-2xl transition-colors duration-300 hover:text-red-500 " />
             </div>
             <div className="rounded-full bg-[#3e1c1f] p-2 flex">
-              <IoShield className="text-2xl hover:text-[#2a9eff] hover:transition-opacity" />
+              <IoShield className="text-2xl  transition-colors duration-300 hover:text-[#2a9eff]" />
             </div>
           </div>
         </div>
-        <div className="text-white flex px-8 py-3 ">
+        <div className="flex text-white col-span-4  px-8 py-3 ">
           <ul className="flex gap-4 justify-between items-center">
             <li>Likes</li>
             <li>Mensajes</li>
@@ -28,7 +28,7 @@ const Pet = () => {
           </ul>
         </div>
       </div>
-      <div className="flex w-full bg-white col-span-1">a</div>
+      <div className="flex w-full bg-[#000000] col-span-1"></div>
     </div>
   );
 };
