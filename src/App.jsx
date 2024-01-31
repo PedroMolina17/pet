6e4f7cc118df347fd6aa76eb7417ab294a8ae5dc
pedromolina17 <pedro.molinanoa@gmail.com>
2024-01-30 19:14:58 -0500

    create git

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Pet from "./components/Pet";
+function App() {
+  return (
+    <div className="text-white">
+      <Pet />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Pet.jsx
@@ -0,0 +1,30 @@
+import { IoShield } from "react-icons/io5";
+import { FaSuitcase } from "react-icons/fa";
+import { IoPawSharp } from "react-icons/io5";
+
+const Pet = () => {
+  return (
+    <div className="flex col-2 w-full">
+      <div className="flex flex-col w-1/4 bg-[#0e1013] col-span-1  ">
+        <div className="flex w-full justify-between items-center  py-7 px-4 bg-gradient-to-tr from-[#fd2878] to-[#ff5c3b]">
+          <div> Usuario </div>
+          <div className="rounded-full flex gap-y-1 gap-x-4 ">
+            <div className="rounded-full bg-[#3e1c1f] p-2 flex">
+              <IoPawSharp className="text-2xl text-white hover:text-[#2a9eff] hover:transition-opacity" />
+            </div>
+            <div className="rounded-full bg-[#3e1c1f] p-2 flex">
+              <FaSuitcase className="text-2xl hover:text-[#2a9eff] hover:transition-opacity" />
+            </div>
+            <div className="rounded-full bg-[#3e1c1f] p-2 flex">
+              <IoShield className="text-2xl hover:text-[#2a9eff] hover:transition-opacity" />
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+      <div className="flex w-full bg-white col-span-1">a</div>
+    </div>
+  );
+};
+
+export default Pet;
